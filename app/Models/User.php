2bacbbfce200a5547a50cb6c15c7a7726b4f9497
Fardan Nozami Ajitama <64827736+CahBantul@users.noreply.github.com>
2024-05-07 12:22:35 +0700

    feat: model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -43,5 +44,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function sendMessage() : HasMany
+    {
+        return $this->hasMany(Message::class, 'sender_id', 'id');
+    }
+
+    public function receiveMessage() : HasMany
+    {
+        return $this->hasMany(Message::class, 'receiver_id', 'id');
     }
 }
